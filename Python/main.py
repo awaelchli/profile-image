53649b430d8c2386cc1e1efac2d9c95da1c04cd3
awaelchli <adrian.waelchli@students.unibe.ch>
2019-08-14 11:51:43 +0200

    refactor, add animated version

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,16 @@
+import os
 from colorsys import hsv_to_rgb
 
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import pi
+
+
+def f(z):
+    """ Inspired by:
+        https://en.wikipedia.org/wiki/Complex_analysis#/media/File:Color_complex_plot.jpg
+    """
+    return (z ** 2 - 1) * (z - 2 - 1j) ** 2 / (z ** 2 + 2 + 2j)
 
 
 def plot(func, size=(128, 128), domain=(-1., 1.)):
@@ -22,18 +30,28 @@ def plot(func, size=(128, 128), domain=(-1., 1.)):
     return rgb
 
 
-def main():
-    """ Inspired by:
-        https://de.wikipedia.org/wiki/Funktionentheorie#/media/Datei:Color_complex_plot2.jpg
-    """
-
-    def f(z):
-        return (z ** 2 - 1) * (z - 2 - 1j) ** 2 / (z ** 2 + 2 + 2j)
-
-    rgb = plot(f, size=(1024, 1024), domain=(-2., 2.))
+def main(height=128):
+    rgb = plot(f, size=(height, height), domain=(-2., 2.))
     rgb = np.flip(rgb, 0)
     plt.imsave('profile-image.png', rgb)
 
 
+def animation(folder='./animation', num_frames=5, height=128):
+    os.makedirs(folder, exist_ok=True)
+    steps = np.linspace(0, 2 * pi, num_frames + 1)
+
+    for i, phi in enumerate(steps[:-1]):
+
+        print(f'Generating frame {i}')
+
+        def g(z):
+            return (np.cos(phi) + 1j * np.sin(phi)) * f(z)
+
+        rgb = plot(g, size=(height, height), domain=(-2., 2.))
+        rgb = np.flip(rgb, 0)
+        plt.imsave(os.path.join(folder, f'{i:03d}.png'), rgb)
+
+
 if __name__ == '__main__':
-    main()
+    main(height=1024)
+    animation(num_frames=50, height=128)
